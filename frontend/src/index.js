b0b3bf57088c419e9149eedf9a5d99ac9bb68bf5
Riya-1508 <119164469+Riya-1508@users.noreply.github.com>
2023-02-02 15:39:10 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -42,7 +42,7 @@ const GraduationCollegePages = () => {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+    {/* <Login /> */}
     {/* <SignUp/> */}
     {/* <Home/> */}
     {/* <PostGraduation/> */}
