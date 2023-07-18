682002c47d399e8bb07b62a8183215e9db9289d2
Riya-1508 <119164469+Riya-1508@users.noreply.github.com>
2023-07-18 13:13:47 +0530

    added flash messages function

--- frontend/src/ForgotPassword.js
+++ frontend/src/ForgotPassword.js
@@ -3,11 +3,18 @@ import "./ForgotPassword.css";
 import Photo_Login from './image/Photo_LoginPage.png';
 import { useState } from "react";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function ForgotPassword() {
     const [email, setEmail] = useState("");
 	const [msg, setMsg] = useState("");
 	const [error, setError] = useState("");
-
+ const diffToast = (message, type) => {
+   toast[type](message, {
+     position: "top-center",
+     theme: "dark",
+   });
+ };
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
@@ -15,6 +22,7 @@ function ForgotPassword() {
 			const { data } = await axios.post(url, { email });
 			setMsg(data.message);
 			setError("");
+            diffToast("Email sent successfully", "success");
 		} catch (error) {
 			if (
 				error.response &&
@@ -25,36 +33,56 @@ function ForgotPassword() {
 				setMsg("");
 			}
             console.log(error)
+            diffToast("Enter correct email", "error");
 		}
 	};
      return (
-        <div className="bg-gradient-to-r from-indigo-100 via-pink-300 to-purple-900 h-screen" >
-        <div className="flex space-x-48 md:flex-shrink-0">
-            <div className="mt-20" >
-                <p className="ml-40 text-3xl font-bold font-serif">Forgot Password</p>
-                    <img className="w-[500px] h-[500px] ml-36"src={Photo_Login} alt=""/>
-            </div>
-                    <div className="border-4 border-white h-screen "></div>
-            <div className="flex justify-end py-36 pr-40">
-                <div className="bg-purple-pink w-96 h-96 rounded-3xl flex flex-col justify-center items-center">
-                    
-                   
-                    <form onSubmit={handleSubmit}>
-                        <div className="my-4">
-                            <label htmlFor="email" className="text-xl text-purple-violent font-bold"  >Email: </label>
-                            <div><input type="email" name="title" className="my-2 shadow appearance border rounded-2xl w-64 py-2 px-3 text-gray-700 leading-tight focus:outline-indigo-100 focus:shadow-outline"  onChange = {(e) => setEmail(e.target.value)} value = {email} ></input>
-                    
-                    </div>
-                        </div>
-                       
-                        
-                        <button type="submit" className="inline-block px-6 py-2.5 bg-purple-100 text-pink-violent font-medium text-lg leading-tight uppercase rounded-full shadow-md hover:bg-white hover:shadow-lg focus:bg-pink-violent focus:text-white focus:shadow-lg focus:outline-none focus:ring-0 active:bg-pink-violent active:text-white active:shadow-lg transition duration-150 ease-in-out" >Submit</button>
-                    </form>
-                    
-                </div>
-            </div>
-        </div >
-        </div >
-    );
+       <div className="bg-gradient-to-r from-indigo-100 via-pink-300 to-purple-900 h-screen">
+         <div className="flex space-x-48 md:flex-shrink-0">
+           <div className="mt-20">
+             <p className="ml-40 text-3xl font-bold font-serif">
+               Forgot Password
+             </p>
+             <img
+               className="w-[500px] h-[500px] ml-36"
+               src={Photo_Login}
+               alt=""
+             />
+           </div>
+           <div className="border-4 border-white h-screen "></div>
+           <div className="flex justify-end py-36 pr-40">
+             <div className="bg-purple-pink w-96 h-96 rounded-3xl flex flex-col justify-center items-center">
+               <form onSubmit={handleSubmit}>
+                 <div className="my-4">
+                   <label
+                     htmlFor="email"
+                     className="text-xl text-purple-violent font-bold"
+                   >
+                     Email:{" "}
+                   </label>
+                   <div>
+                     <input
+                       type="email"
+                       name="title"
+                       className="my-2 shadow appearance border rounded-2xl w-64 py-2 px-3 text-gray-700 leading-tight focus:outline-indigo-100 focus:shadow-outline"
+                       onChange={(e) => setEmail(e.target.value)}
+                       value={email}
+                     ></input>
+                   </div>
+                 </div>
+
+                 <button
+                   type="submit"
+                   className="inline-block px-6 py-2.5 bg-purple-100 text-pink-violent font-medium text-lg leading-tight uppercase rounded-full shadow-md hover:bg-white hover:shadow-lg focus:bg-pink-violent focus:text-white focus:shadow-lg focus:outline-none focus:ring-0 active:bg-pink-violent active:text-white active:shadow-lg transition duration-150 ease-in-out"
+                 >
+                   Submit
+                 </button>
+                 <ToastContainer />
+               </form>
+             </div>
+           </div>
+         </div>
+       </div>
+     );
 }
 export default ForgotPassword;
